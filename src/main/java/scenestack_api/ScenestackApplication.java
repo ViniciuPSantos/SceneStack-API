f48dd46907f19b2026a3af7b99e94db75da2a4a2
ViniciuPSantos <vipaulasantos@gmail.com>
2026-05-24 23:47:51 -0300

    Adicionando os primeiros arquivos

---
+++ src/main/java/scenestack_api/ScenestackApplication.java
@@ -0,0 +1,13 @@
+package scenestack_api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ScenestackApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ScenestackApplication.class, args);
+	}
+
+}
---
+++ src/test/java/scenestack_api/ScenestackApplicationTests.java
@@ -0,0 +1,13 @@
+package scenestack_api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ScenestackApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
